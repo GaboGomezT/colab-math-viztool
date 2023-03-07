25e831f10c3b5b4662327b5b46962aea003096b6
GaboGomezT <gomezgabriel09@gmail.com>
2023-03-07 12:36:12 -0600

    created Login UI

---
+++ frontend/src/components/Login/Login.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Login.module.css"
+
+export default function Login() {
+  return (
+    <main>
+      <h1>Iniciar Sesión</h1>
+      <form action="#">
+        <label htmlFor="email">Correo</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Contraseña</label>
+        <input type="password" name="password" id="password" />
+        <button>Entrar</button>
+      </form>
+      <p>Crear cuenta</p>
+    </main>
+  );
+}
