610512e9f69d0b444181b5b8b5465e8992821136
GaboGomezT <gomezgabriel09@gmail.com>
2023-03-07 20:17:33 -0600

    code format

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-  } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Login from './components/Login/Login'
-import './index.css'
+import Login from "./components/Login/Login";
+import "./index.css";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Login />,
-    },
-  ]);
-  
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <RouterProvider router={router} />
-    </React.StrictMode>
-  );
+  {
+    path: "/",
+    element: <Login />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
