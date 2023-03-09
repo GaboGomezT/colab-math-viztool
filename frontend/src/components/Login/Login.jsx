e9d7d27b6fd3d868db02f6f2d5cd2094cce65aca
GaboGomezT <gomezgabriel09@gmail.com>
2023-03-09 17:03:28 -0600

    login integration

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,15 +1,58 @@
 import React from "react";
+import { useState } from "react";
 import "./Login.module.css";
 
 export default function Login() {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+	});
+
+	function handleChange(event) {
+		setFormData((prevFormData) => {
+			return {
+				...prevFormData,
+				[event.target.name]: event.target.value,
+			};
+		});
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		fetch(`${import.meta.env.VITE_BACKEND_API_URL}/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(formData),
+		})
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+				return response.json();
+			})
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error("There was a problem with the fetch operation:", error);
+			});
+	}
+
 	return (
 		<main>
 			<h1>Iniciar Sesión</h1>
-			<form action="#">
+			<form onSubmit={handleSubmit}>
 				<label htmlFor="email">Correo</label>
-				<input type="email" name="email" id="email" />
+				<input type="email" onChange={handleChange} name="email" id="email" />
 				<label htmlFor="password">Contraseña</label>
-				<input type="password" name="password" id="password" />
+				<input
+					type="password"
+					onChange={handleChange}
+					name="password"
+					id="password"
+				/>
 				<button>Entrar</button>
 			</form>
 			<p>Crear cuenta</p>
