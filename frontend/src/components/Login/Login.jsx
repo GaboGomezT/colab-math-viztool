3ca077e95faffb1c3eab68e96024599fdd3c008c
GaboGomezT <gomezgabriel09@gmail.com>
2023-03-09 16:32:42 -0600

    frontend reformat using tabs

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import "./Login.module.css";
 
 export default function Login() {
-  return (
-    <main>
-      <h1>Iniciar Sesión</h1>
-      <form action="#">
-        <label htmlFor="email">Correo</label>
-        <input type="email" name="email" id="email" />
-        <label htmlFor="password">Contraseña</label>
-        <input type="password" name="password" id="password" />
-        <button>Entrar</button>
-      </form>
-      <p>Crear cuenta</p>
-    </main>
-  );
+	return (
+		<main>
+			<h1>Iniciar Sesión</h1>
+			<form action="#">
+				<label htmlFor="email">Correo</label>
+				<input type="email" name="email" id="email" />
+				<label htmlFor="password">Contraseña</label>
+				<input type="password" name="password" id="password" />
+				<button>Entrar</button>
+			</form>
+			<p>Crear cuenta</p>
+		</main>
+	);
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,14 +6,14 @@ import Login from "./components/Login/Login";
 import "./index.css";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login />,
-  },
+	{
+		path: "/",
+		element: <Login />,
+	},
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
 );
