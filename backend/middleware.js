92b42f8e00b8df4a39e34b3473629f8c2c7628d7
GaboGomezT <gomezgabriel09@gmail.com>
2023-03-09 10:38:25 -0600

    tested protected URL

--- backend/middleware.js
+++ backend/middleware.js
@@ -7,18 +7,18 @@ const secretKey = process.env.ACCESS_TOKEN_SECRET;
 
 // Middleware function to verify the token
 const verifyToken = (req, res, next) => {
-  const token = req.headers["x-access-token"];
-  if (!token) {
-    return res.status(401).send("Access denied. No token provided.");
-  }
+	const token = req.headers["x-access-token"];
+	if (!token) {
+		return res.status(401).send("Access denied. No token provided.");
+	}
 
-  try {
-    const decoded = jwt.verify(token, secretKey);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    res.status(400).send("Invalid token.");
-  }
+	try {
+		const decoded = jwt.verify(token, secretKey);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		res.status(400).send("Invalid token.");
+	}
 };
 
 module.exports = verifyToken;
